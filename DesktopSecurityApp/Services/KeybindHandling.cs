bd841d6d7ff6432f446df21587b5eb7d38bd4482
hame997 <muhamed.ajanovic@edu.fit.ba>
2024-01-23 16:20:00 +0100

    Added Keybind handler

---
+++ DesktopSecurityApp/Services/KeybindHandling.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DesktopSecurityApp.Services
+{
+    internal class KeybindHandling
+    {
+        public static void RegisterKeyBindings(Window window)
+        {
+            EventManager.RegisterClassHandler(typeof(Window), Window.KeyDownEvent,new KeyEventHandler(KeyBindHandler));
+        }
+        private static void KeyBindHandler(object sender, KeyEventArgs e)
+        {
+            // Provera key-bind kombinacije (Ctrl + Alt + S)
+            if ( e.Key == Key.S)
+            {
+                // Ako je key-bind ispravan, izvršite željene akcije
+                MessageBox.Show("Key-bind uspešno pritisnut!");
+                // Dodajte ovde dodatni kôd ili pozovite funkciju koja će se izvršiti
+            }
+        }
+    }
+}
+
