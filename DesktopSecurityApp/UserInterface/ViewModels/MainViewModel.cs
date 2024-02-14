c0138bcdeb275e3104592f3620c9734d9f4cf12a
AdnanDz251 <adnan.dzindo1507@gmail.com>
2024-02-14 19:00:07 +0100

    desktop UI

---
+++ DesktopSecurityApp/UserInterface/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DesktopSecurityApp.UserInterface.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSecurityApp.UserInterface.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel() 
+        {
+            
+        }
+    }
+}
