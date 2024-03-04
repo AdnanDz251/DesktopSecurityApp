9bf8e5fd3f696503254c4b4711a2918e2422e69e
hame997 <muhamed.ajanovic@edu.fit.ba>
2024-03-04 18:16:28 +0100

    Added User Update Method

--- DesktopSecurityApp/Services/UserUpdater.cs
+++ DesktopSecurityApp/Services/UserUpdater.cs
@@ -1,38 +1,12 @@
-﻿using DesktopSecurityApp.Models;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DesktopSecurityApp.Models;
+using Newtonsoft.Json;
 
 namespace DesktopSecurityApp.Services
 {
-    public static class UserInformationManagement
+    public static class UserUpdater
     {
-        public static void SaveUserInfoToJsonFile()
-        {
-            // Instanciranje objekta UserInfo s korisničkim informacijama
-            UserInfo userInfo = new UserInfo
-            {
-                Username = "example_user",
-                Key = "L",
-                Email = "example@example.com"
-            };
-
-            // Dobijte apsolutnu putanju do direktorija izvršne datoteke aplikacije
-            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
-
-            string customFolderPath = Path.Combine(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(executablePath).FullName).FullName).FullName).FullName, "Data", "UserData");
-            Directory.CreateDirectory(customFolderPath);
-
-            // Putanja do JSON datoteke "user_data.json" unutar direktorija "UserData"
-            string jsonFilePath = Path.Combine(customFolderPath, "user_data.json");
-
-            // Spremanje korisničkih informacija u JSON datoteku
-            JsonEncryptionDecryption.EncryptToJsonFile(userInfo, jsonFilePath);
-        }
         public static void UpdateUserInfoInJsonFile(UpdateUserInfo updatedUserInfo)
         {
             // Dobivanje putanje do JSON datoteke
