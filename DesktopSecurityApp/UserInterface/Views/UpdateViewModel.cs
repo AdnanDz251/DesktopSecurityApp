86728f51dc97cb412896a3f7ba3ebe48d8e26501
hame997 <muhamed.ajanovic@edu.fit.ba>
2024-03-13 19:46:39 +0100

    2.0 version

--- DesktopSecurityApp/UserInterface/Views/UpdateViewModel.cs
+++ DesktopSecurityApp/UserInterface/Views/UpdateViewModel.cs
@@ -1,9 +1,9 @@
-﻿using DesktopSecurityApp.Services;
+﻿using DesktopSecurityApp.Models;
+using DesktopSecurityApp.Services;
 using DesktopSecurityApp.UserInterface.Core;
-using System;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
 using System.Windows.Input;
 
 namespace DesktopSecurityApp.UserInterface.Views
@@ -11,6 +11,9 @@ namespace DesktopSecurityApp.UserInterface.Views
     public class UpdateViewModel : INotifyPropertyChanged
     {
         private string _activationKey;
+        private string _newUsername;
+        private string _newKey;
+        private string _newEmail;
 
         public string ActivationKey
         {
@@ -25,6 +28,8 @@ namespace DesktopSecurityApp.UserInterface.Views
             }
         }
 
+        // Dodajte svojstva za nove podatke korisnika (npr. NewUsername, NewKey, NewEmail)
+
         public ICommand SaveCommand { get; }
 
         public UpdateViewModel()
@@ -37,6 +42,37 @@ namespace DesktopSecurityApp.UserInterface.Views
         {
             // Ovdje dodajte logiku za čuvanje aktivacione ključne riječi
             KeybindHandling.SetActivationKey((Key)new KeyConverter().ConvertFromString(ActivationKey));
+
+            // Poziv metode za ažuriranje korisničkih podataka
+            UpdateUserInfo();
+        }
+
+        private void UpdateUserInfo()
+        {
+            // Prvo kreirajte objekat sa novim informacijama korisnika
+            var updatedUserInfo = new UpdateUserInfo
+            {
+                NewUsername = _newUsername,
+                NewKey = _newKey,
+                NewEmail = _newEmail
+            };
+
+            // Zatim pozovite metodu za ažuriranje korisničkih podataka u JSON datoteci
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+            string customFolderPath = Path.Combine(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(executablePath).FullName).FullName).FullName).FullName, "Data", "UserData");
+
+            try
+            {
+                UserUpdater.UpdateUserInfoInJsonFile(updatedUserInfo, customFolderPath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                // Obrada greške ako datoteka nije pronađena
+            }
+            catch (Exception ex)
+            {
+                // Obrada ostalih grešaka
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
