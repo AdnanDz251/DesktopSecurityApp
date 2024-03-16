61faa2bfc9cb0d803d7b5ea2b4e06a16fda97cc4
FiggyHunter <leonardo.roi3@gmail.com>
2024-03-16 09:17:06 +0100

    Key is now dynamic

--- DesktopSecurityApp/Services/KeybindHandling.cs
+++ DesktopSecurityApp/Services/KeybindHandling.cs
@@ -24,7 +24,7 @@ namespace DesktopSecurityApp.Services
 
         public static OverlayWindow overlayWindow;
 
-        private static Key activationKey = Key.S; // Default activation key is 'S'
+        private static Key activationKey = (Key)Enum.Parse(typeof(Key), DesktopSecurityApp.Services.UserInformationManagement.GetJSONFile().Key);
         public static void RegisterKeyBindings(Window window)
         {
             EventManager.RegisterClassHandler(typeof(Window), Window.KeyDownEvent, new KeyEventHandler(KeyBindHandler));
@@ -76,7 +76,7 @@ namespace DesktopSecurityApp.Services
             overlayWindow = new OverlayWindow();
             overlayWindow.Show();
         }
-        private static void CloseOverlayWindow()
+        public static void CloseOverlayWindow()
         {
             overlayWindow.Close();
             overlayWindow = null;
