acfab0be4d58f297c8b7340ba00c933b9666bd67
FiggyHunter <leonardo.roi3@gmail.com>
2024-03-16 09:16:31 +0100

    Bug Fix: JSON not decoding properly

--- DesktopSecurityApp/Services/JsonEncryptionDecryption.cs
+++ DesktopSecurityApp/Services/JsonEncryptionDecryption.cs
@@ -4,10 +4,13 @@ using System.IO;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows;
 using Newtonsoft.Json;
+using System.Text.Json;
 
-namespace DesktopSecurityApp.Services
-{
+
+namespace DesktopSecurityApp.Services 
+{ 
     public static class JsonEncryptionDecryption
     {
         // Ključ za enkripciju/dekripciju. Preporučuje se korištenje sigurnog i nasumičnog ključa.
@@ -31,10 +34,11 @@ namespace DesktopSecurityApp.Services
             byte[] encryptedData = File.ReadAllBytes(filePath);
 
             // Dekriptovanje podataka
-            string decryptedJson = DecryptStringFromBytes(encryptedData, Key);
+            string decryptedJson = DecryptStringFromBytes(encryptedData, Key).ToString();
+            string jsonAsString = JsonConvert.SerializeObject(decryptedJson);
 
             // Deserijalizacija JSON podataka u objekat
-            return JsonConvert.DeserializeObject<T>(decryptedJson);
+            return JsonConvert.DeserializeObject<T>(jsonAsString);
         }
 
         public static byte[] EncryptStringToBytes(string plainText, byte[] key)
